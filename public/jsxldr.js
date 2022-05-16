16eaa16ac46d5aca765117e82f7812230642df64
ftukmakov <ftukmakov@stech.ru>
2022-05-16 22:19:37 +0300

    css [A]/[-A] for nth(-last)-of-type(An+B) alias worked. $scope selects in work

--- public/jsxldr.js
+++ public/jsxldr.js
@@ -1,9 +1,10 @@
 (function(){
-const nextId=0;
+let nextId=0;
 const map= {};    
 const parse=(BEM,str)=>str.replace(/((= |=|="|= ")|((^|\s)\.))((__[a-z])|__([^a-z]|$))/gi,`$2$3${BEM}$6$7`)
-    .replace(/\[((\d+)n|n(\d+))(\+\d+)?\]/g,`:nth-of-type($2$3n$4)`) //shortcut for :nth-of-type(An+B) by just [An+B] (or [nA+B] //linter fine form)
-    .replace(/\[((\d+)n|n(\d+))-(\d+)?\]/g,`:nth-last-of-type($2$3n+$4)`) //same for :nth-last-of-type(An+B) by [An-B]/[nA-B] (B must exist even if 0)
+    .replace(/\[(\d*)\]/,`[0n+$1]`).replace(/\[-(\d*)\]/,`[0n-$1]`)         //option for replace [A] / [-A] to [0n+A] / [0n-A]
+        .replace(/\[((\d+)n|n(\d+))(\+\d+)?\]/g,`:nth-of-type($2$3n$4)`)      //shortcut for :nth-of-type(An+B) by just [An+B] (or [nA+B] //linter fine form)
+        .replace(/\[((\d+)n|n(\d+))-(\d+)?\]/g,`:nth-last-of-type($2$3n+$4)`) //same for :nth-last-of-type(An+B) by [An-B]/[nA-B] (B must exist even if 0)
 const EVENT_TYPES=['_click','_dblclick','_change'];
 
 function jsxldr($pa, $pa_children){
@@ -14,24 +15,25 @@ function jsxldr($pa, $pa_children){
         let[,jsxClass,href,ext]=/(?:([^:]*):)?([^:]+?)(\.[^.]*)?$/.exec(jsxldrAttr)??[]
         jsxClass??=href; href+=ext??='.htm';
         if(!href || !jsxClass || /\./.test(jsxClass))return;
-        el.removeAttribute('_class'); el.classList.add(jsxClass); el.setAttribure('jsx',_id);
+        el.removeAttribute('_class'); el.classList.add(jsxClass); el.setAttribute('jsx',_id);
         const _children={};
         const $= Object.defineProperties(
             q=>{
                 if(q===undefined) return jsxldr.call(el,$,_children) //shortcut $() for refresh this
                 if(q=='.__')return el;                               //shortcut $('.__') for return this dom element
                 let[sel,scope]=q.split(/\$(?=[^\]]*$)/)
-                if(scope!=undefined){
-                    q+='[jsx]';
-                    scope= scope.split('.')
-                }
-                q= el.querySelectorAll(
-                    (jsxClass[0]=='>'?':scope':'')+       //shortcut for :scope> by starting with '>', without typing :scope
-                    parse(jsxClass,q)                     //replace '__' pattern in class literals to this jsxClass string
-                    //if after all finding $(prop(.prop(.prop(...)))) resolving dom nodes to scope object prop
-                )
+                console.log({sel,scope});
+                if(scope!=undefined)sel+='[jsx]';
+                if(sel[0]=='>')sel=':scope'+sel       //shortcut for :scope> by starting with '>', without typing :scope
+                q= parse(jsxClass,sel)            //replace '__' pattern in class literals to this jsxClass string
+
+                console.log(`${$}.querySelectorAll(${q})`);
+                q= [].slice.call(el.querySelectorAll(q))
+                console.log('=',q);
                 if(scope){
-                    q= q.flatMap(jsx=>{
+                    scope= scope.split('.')
+                    //if after all finding $(prop(.prop(.prop(...)))) resolving dom nodes to scope object prop
+                    q= [].flatmap.call(q,jsx=>{
                         jsx=jsx.getAttribute('jsx');
                         console.log({jsx});
                         if(jsx in map)return [map[jsx]]
@@ -44,29 +46,30 @@ function jsxldr($pa, $pa_children){
             parent:{value:$pa},            //TODO: JSX scope pa
             children:{value:BEM=> BEM? _children[BEM].slice(): _children},
             toString:{value:f=> jsxClass},
-            removeChild:{value:$ch=>{
-                console.log(`${$}.removeChild(${$ch})`,$ch);
-                if(typeof $ch=='string'){
-                    let ch= $($ch);
-                    if(!ch)return null
-                    else{
-                        let[...match]=ch.className.match(RegExp(Object.keys(_children).join('|') ));
-                        console.log({match});
-                        if(match.length>1)throw new Error("removeChild by ambiguous selector")
-                        if(match=match[0]){
-                            $ch = _children[match].find($ch => $ch('.__') === ch);
-                            if (!$ch) return null;
-                            let i = _children[$ch].indexOf($ch);
-                            _children[$ch].splice(i,1);
-                        }
-                        return el.removeChild(ch);
+            removeChild:{value:$chs=>{
+                console.log(`${$}.removeChild(${$chs})`,$chs);
+                if(typeof $chs=='string') $chs= $($chs);
+                console.log({$chs});
+                if(!$chs)return null;
+                $chs=[$chs].flat()    //support for removeChild([$ch1,$ch2,...,$chN]) same as removeChild($ch)
+                console.log({$chs});
+
+                let dels=$chs.flatMap($ch=>{
+                    if(typeof $ch=='function'){
+                        console.log('removing $ch()')
+                        let i = _children[$ch].indexOf($ch);
+                        if(!~i)return[];
+                        let id=$ch('.__').getAttribute('jsx');
+                        delete map[id];
+                        el.removeChild($ch('.__'));
+                        return _children[$ch].splice(i,1)
                     }
-                }else if(typeof $ch=='function'){
-                    let i = _children[$ch].indexOf($ch);
-                    if(!~i)return null;
-                    el.removeChild($ch('.__'));
-                    return _children[$ch].splice(i,1)[0];
-                }else return null
+                    console.log('removing ch',$ch, 'from parent', $ch.parentElement);
+                    try{return[$ch.parentElement.removeChild($ch)]}catch{return[]}
+                })
+                console.log({dels});
+                dels.map(del=>del.destruct?.());
+                return dels.length?dels:null
             }},
             tree:{value:(t,i)=>`${'\t'.repeat(t)}${$}[${i|0}]:{\n${[
                     ...Object.keys($).map(k => `${'\t'.repeat(-~t)}$${k}:${$[k]}`),
@@ -76,7 +79,7 @@ function jsxldr($pa, $pa_children){
                 ].join(`,${'\t'.repeat(t)}\n`)
             }}`},
         })
-        map[id]=$;
+        map[_id]=$;
         if($pa_children)$pa_children[$]=($pa_children[$]??[]).concat($)
 
         await fetch(href).then(r=>r.text()).then(async str=>{
@@ -95,8 +98,9 @@ function jsxldr($pa, $pa_children){
             //binds:
             bindlate.forEach(ch=>{
                 EVENT_TYPES.forEach(ev=>{
-                    let attr= ch.getAttribute(ev);
+                    let attr= ch.getAttribute(ev) ;
                     if(!attr)return;
+                    //TODO: by $ scope
                     let[v,sel,...bind]=/([.#$][^.#$]*)([.#$][^.#$]*)?([.#$][^.#$]*)?/.exec(attr)??[];
                     let $pa=$;
                     for(;v=bind.shift();sel=v) $pa= $pa.children(sel.slice(1) )[0];
